fd6351e6de7253ee7f4025ac63b11c9b4ffd3da8
pso2ngs <130035303+pso2ngs@users.noreply.github.com>
2023-04-06 16:00:22 +0900

    Create srvc.js

---
+++ srvc.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'pwa-sample-caches-v1';
+// Cache targets
+const urlsToCache = [
+  './',
+  './index.html',
+  './pwa.json',
+  './google.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response ? response : fetch(event.request);
+      })
+  );
+});
