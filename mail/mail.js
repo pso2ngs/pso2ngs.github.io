3b8396e31d67f6b7dec3bce5889d849ca2a6560d
NoobHobbyist <130035303+pso2ngs@users.noreply.github.com>
2023-09-14 08:57:50 +0900

    Update mail.js

--- mail/mail.js
+++ mail/mail.js
@@ -1,9 +1,3 @@
-var room;
-onload = () => {
-  document.querySelector('button').onclick = () => {
-    if(document.querySelector('#cre').value != '') {
-    } else if(document.querySelector('#ent').value != '') {
-    } else {
-      alert('ルーム名を入力してください');
-    }
-}
+self.addEventListener('message', function (event) {
+  self.registration.showNotification(event.data);
+});
