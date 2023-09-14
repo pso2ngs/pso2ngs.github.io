f0f60a2c910b62f9cdf532f791497bce2f567909
NoobHobbyist <130035303+pso2ngs@users.noreply.github.com>
2023-09-14 09:02:29 +0900

    Update noti.js

--- mail/noti.js
+++ mail/noti.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', () => {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('mail.js').then(registration => 
-      console.log('registered', registration))
-      .catch(error => console.log('error', error));
+      alert(registration))
+      .catch(error => alert(error));
   }
   document.getElementById('send').addEventListener('click', () => {
     if (Notification.permission === 'granted') {
