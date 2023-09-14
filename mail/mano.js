72d01f41c9612ba836a30d863aa920aca3b60e0a
NoobHobbyist <130035303+pso2ngs@users.noreply.github.com>
2023-09-14 15:33:25 +0900

    Update mano.js

--- mail/mano.js
+++ mail/mano.js
@@ -1,2 +1,11 @@
 onload = () => {
-  
+  Notification.requestPermission((result) => {
+    if (result === "granted") {
+      navigator.serviceWorker.ready.then((registration) => {
+        registration.showNotification("あべけんはA組の誰かが好きらしい", {
+          body: "あべけんはA組の誰かが好きらしいw"
+        });
+      });
+    }
+  });
+}
