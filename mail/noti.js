24dfc2449bcea8f1cb18e120ae2accebb527a5bf
NoobHobbyist <130035303+pso2ngs@users.noreply.github.com>
2023-09-16 14:45:45 +0900

    Update noti.js

--- mail/noti.js
+++ mail/noti.js
@@ -1,34 +1,3 @@
 window.addEventListener('load', () => {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('mail.js').then(registration => {
-      alert(registration + 'a');
-    });
-  }
-  /*document.onclick = () => {
-    Notification.requestPermission().then((permission) => {
-      if (permission === "granted") {
-        setInterval(() => {
-          const notification = new Notification("あべけんはA組の誰かが好きらしいw");
-        }, 1);
-      }
-    });
-  }*/
-  /*Notification.requestPermission((result) => {
-    if (result === "granted") {
-      navigator.serviceWorker.ready.then((registration) => {
-        registration.showNotification("あべけんはA組の誰かが好きらしい", {
-          body: "あべけんはA組の誰かが好きらしいw"
-        });
-      });
-    }
-  });*/
-  document.body.addEventListener('click', () => {
-    if (Notification.permission === 'granted') {
-      setInterval(() => {
-        navigator.serviceWorker.ready.then(registration => {
-          registration.active.postMessage('あべけんはA組の誰かが好きらしい');
-        });
-      }, 1);
-    }
-  });
+  const ww = new Worker('mail.js');
 });
