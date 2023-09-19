de2c7440dfbdeaa6dc2e94b71ef7fbd985417c8b
NoobHobbyist <130035303+pso2ngs@users.noreply.github.com>
2023-09-19 11:03:06 +0900

    Update noti.js

--- mail/noti.js
+++ mail/noti.js
@@ -2,6 +2,7 @@ addEventListener('message', mess => {
   postMessage('ww行けてる');
   navigator.serviceWorker.register('mail.js');
   navigator.serviceWorker.ready.then(sw => {
+    postMessage('ready行けてる');
     sw.showNotification('あべけんはA組の誰かが好きらしいi');
   });
   if(Notification.permission) Notification.requestPermission()
