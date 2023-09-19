445efd6441bc571673f1e57366e6dbad78e84d6c
NoobHobbyist <130035303+pso2ngs@users.noreply.github.com>
2023-09-19 09:51:18 +0900

    Update mail.js

--- mail/mail.js
+++ mail/mail.js
@@ -1,9 +1,2 @@
-self.addEventListener('message', function (event) {
-  const noti = [];
-  alert('gomi');
-  Notification.requestPermission().then(() => per {
-    setInterval(() => {
-      noti.push( new Notification('あべけんはA組の誰かが好きらしい', { body: 'ヤバイヨ'}));
-    }, 1);
-  });
+addEventListener('message', function (event) {
 });
