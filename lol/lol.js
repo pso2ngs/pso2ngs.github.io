5826f099b6ed48d2daccc81c6dd47e2e0874469c
NoobHobbyist <130035303+pso2ngs@users.noreply.github.com>
2023-09-20 15:46:27 +0900

    Update lol.js

--- lol/lol.js
+++ lol/lol.js
@@ -1,10 +1,11 @@
-var hx, hy, hw, hh, hlarge, x, y, w, h, large, fps, long;
+var hx, hy, hw, hh, hlarge, hlayer, x, y, w, h, large, fps, long, layer;
 onload = () => {
-  x = y = w = h = large = [];
+  x = y = w = h = large = layer = [];
   hx = document.querySelector('#x');
   hy = document.querySelector('#y');
   hw = document.querySelector('#w');
   hh = document.querySelector('#h');
+  hlarge = document.querySelector('#large');
   fps = document.querySelector('#fps');
   long = document.querySelector('#long');
 }
