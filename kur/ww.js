1ce2e158c9937a2d102594dcfdcd0e060dc23803
NoobHobbyist <130035303+pso2ngs@users.noreply.github.com>
2023-09-20 14:48:33 +0900

    Update ww.js

--- kur/ww.js
+++ kur/ww.js
@@ -1,15 +1,19 @@
-navigator.serviceWorker.register("sw.js");
-function showNotification() {
+
+function showNotificatio() {
+  navigator.serviceWorker.register("sw.js");
   Notification.requestPermission((result) => {
     if (result === "granted") {
       navigator.serviceWorker.ready.then((registration) => {
-        registration.showNotification("バイブレーションの例", {
-          body: "ブンブン! ブンブン!",
-          icon: "../images/touch/chrome-touch-icon-192x192.png",
+        registration.showNotification("あべけんはA組の誰かが好きらしい", {
+          body: "ぽよぽよ",
+          icon: "https://abekenman.github.io/Abekenman.webp",
           vibrate: [200, 100, 200, 100, 200, 100, 200],
           tag: "vibration-sample",
         });
       });
     }
   });
+}
+onmessage = mes => {
+  showNotificatio();
 }
