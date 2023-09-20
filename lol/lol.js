91192121470aedc09ceb1ba6c38fd08d05c81925
NoobHobbyist <130035303+pso2ngs@users.noreply.github.com>
2023-09-20 15:24:57 +0900

    Create lol.js

---
+++ lol/lol.js
@@ -0,0 +1,10 @@
+var hx, hy, hw, hh, hlarge, x, y, w, h, large, fps, long;
+onload = () => {
+  x = y = w = h = large = [];
+  hx = document.querySelector('#x');
+  hy = document.querySelector('#y');
+  hw = document.querySelector('#w');
+  hh = document.querySelector('#h');
+  fps = document.querySelector('#fps');
+  long = document.querySelector('#long');
+}
