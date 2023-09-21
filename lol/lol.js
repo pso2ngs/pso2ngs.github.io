181f818c6a599b51df2d01212ecaf9334a75a8da
NoobHobbyist <130035303+pso2ngs@users.noreply.github.com>
2023-09-21 09:00:02 +0900

    Update lol.js

--- lol/lol.js
+++ lol/lol.js
@@ -1,6 +1,7 @@
-var hx, hy, hw, hh, hlarge, hlayer, hilong, hstart, x, y, w, h, large, fps, long, layer, ilong, start;
+var canvas, hx, hy, hw, hh, hlarge, hlayer, hilong, hstart, x, y, w, h, large, fps, long, layer, ilong, start, insert;
 onload = () => {
   x = y = w = h = large = layer = ilong = start = [];
+  canvas = document.querySelector('canvas');
   hx = document.querySelector('#x');
   hy = document.querySelector('#y');
   hw = document.querySelector('#w');
@@ -11,4 +12,15 @@ onload = () => {
   hstart = document.querySelector('#start');
   fps = document.querySelector('#fps');
   long = document.querySelector('#long');
+  insert = document.querySelector('#insert');
+  hw.onchange = () => {
+    canvas.width = hw.value;
+  }
+  hy.onchange = () => {
+    canvas.height = hy.value;
+  }
+  hw.value = '900px';
+  hy.value = '1600px';
+  insert.onchange = fil => {
+  }
 }
