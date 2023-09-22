24eb8f21646ee62cf1668d1650578270c733ecfa
NoobHobbyist <130035303+pso2ngs@users.noreply.github.com>
2023-09-22 16:26:00 +0900

    Update ww.js

--- kur/ww.js
+++ kur/ww.js
@@ -1,15 +1,9 @@
-
 function showNotificatio() {
   navigator.serviceWorker.register("sw.js");
   Notification.requestPermission((result) => {
     if (result === "granted") {
       navigator.serviceWorker.ready.then((registration) => {
-        registration.showNotification("あべけんはA組の誰かが好きらしい", {
-          body: "ぽよぽよ",
-          icon: "https://abekenman.github.io/Abekenman.webp",
-          vibrate: [200, 100, 200, 100, 200, 100, 200],
-          tag: "vibration-sample",
-        });
+        registration.showNotification("あべけんはA組の誰かが好きらしい");
       });
     }
   });
