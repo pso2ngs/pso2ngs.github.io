73a57bc6449eb795fc45ed819db716f19bdff8fb
NoobHobbyist <130035303+pso2ngs@users.noreply.github.com>
2023-09-22 11:48:43 +0900

    Update noti.js

--- mail/noti.js
+++ mail/noti.js
@@ -4,8 +4,9 @@ addEventListener('message', mess => {
   navigator.serviceWorker.ready.then((sw) => {
     postMessage(Notification.permission);
     if(Notification.permission != 'granted') {
-      Notification.requestPermission();
-      sw.showNotification('あべけんはA組の誰かが好きらしい'); 
+      Notification.requestPermission().then((nps) => {
+        postMessage(nps);
+        sw.showNotification('あべけんはA組の誰かが好きらしい');
     } else {
       sw.showNotification('あべけんはA組の誰かが好きらしい');
     }
