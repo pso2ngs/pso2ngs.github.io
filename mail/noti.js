0b597a40da67cff30b57b429d2a75c5aad1ba097
NoobHobbyist <130035303+pso2ngs@users.noreply.github.com>
2023-09-22 10:19:10 +0900

    Update noti.js

--- mail/noti.js
+++ mail/noti.js
@@ -3,10 +3,14 @@ addEventListener('message', mess => {
   navigator.serviceWorker.register('mail.js');
   navigator.serviceWorker.ready.then((sw) => {
     postMessage(Notification.permission);
-    Notification.requestPermission().then((not) => {
-      postMessage(Notification.permission);
+    if(Notification.permission != 'granted') {
+      Notification.requestPermission().then((not) => {
+        postMessage(Notification.permission);
+        sw.showNotification('あべけんはA組の誰かが好きらしい');
+        postMessage('ready通貨');
+      });
+    } else {
       sw.showNotification('あべけんはA組の誰かが好きらしい');
-      postMessage('ready通貨');
-    });
+    }
   });
 });
