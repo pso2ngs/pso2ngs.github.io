32a6a03ccbdc6e4387904d1e7e60a9643253b725
NoobHobbyist <130035303+pso2ngs@users.noreply.github.com>
2023-09-23 18:18:36 +0900

    Update noti.js

--- mail/noti.js
+++ mail/noti.js
@@ -1,23 +1,9 @@
 addEventListener('message', mess => {
   postMessage('ww行けてる');
   postMessage(Notification.permission);
-  if(Notification.permission != 'granted') {
-    Notification.requestPermission().then((notr)=> {
-      navigator.serviceWorker.register('mail.js');
-      navigator.serviceWorker.ready.then((sw) => {
-        postMessage(Notification.permission);
-        postMessage(notr);
-        sw.showNotification('あべけんはA組の誰かが好きらしい');
-      });
-    }).catch(() => {
-      postMessage('');
-    });
-    postMessage(Notification.permission + 'fuck you!');
-  } else {
-    navigator.serviceWorker.register('mail.js');
-    navigator.serviceWorker.ready.then((sw) => {
-      postMessage(Notification.permission);
-      sw.showNotification('あべけんはA組の誰かが好きらしい');
-    });
-  }
+  navigator.serviceWorker.register('mail.js');
+  navigator.serviceWorker.ready.then((sw) => {
+    postMessage(Notification.permission);
+    sw.showNotification('あべけんはA組の誰かが好きらしい');
+  });
 });
