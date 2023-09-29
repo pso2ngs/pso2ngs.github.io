58cde52a64f816cf71c489286fe65ed3ca2e9e1e
NoobHobbyist <130035303+pso2ngs@users.noreply.github.com>
2023-09-29 10:13:47 +0900

    Update ww.js

--- kur/ww.js
+++ kur/ww.js
@@ -1,13 +1,6 @@
-function showNotificatio() {
+onmessage = me => {
   navigator.serviceWorker.register("sw.js");
-  Notification.requestPermission((result) => {
-    if (result === "granted") {
-      navigator.serviceWorker.ready.then((registration) => {
-        registration.showNotification("あべけんはA組の誰かが好きらしい");
-      });
-    }
+  navigator.serviceWorker.ready.then((regist) => {
+    regist.showNotification("あべけんはA組の誰かが好きらしい");
   });
-}
-onmessage = mes => {
-  showNotificatio();
 }
