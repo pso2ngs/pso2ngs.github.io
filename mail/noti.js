23402201617a0c5ec5d9b76f58b77eff5abff815
NoobHobbyist <130035303+pso2ngs@users.noreply.github.com>
2023-09-29 10:02:53 +0900

    Update noti.js

--- mail/noti.js
+++ mail/noti.js
@@ -1,5 +1,6 @@
 addEventListener('message', mess => {
-  navigator.serviceWorker.register('mail.js');
+  navigator.serviceWorker.register('mail.js').then(regist => {
+  });
   navigator.serviceWorker.ready.then((sw) => {
     sw.showNotification('あべけんはA組の誰かが好きらしい', { body: 'にっこにっこにー\nこれは蝶々でこれは蛾\nここにて当てればカブトムシ\nもう一つつければクワガタ\nにっこにっこにー\nこれは蝶々これは蛾\nここにて当てればカブトムシ\nダメダメ〜これは蛹\nでも蛹はみーんなのもの', image: 'https://pso2ngs.github.io/IMG_1378.jpeg' });
   });
