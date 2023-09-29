0e77977cb637683379412273df3c11c730d30f72
NoobHobbyist <130035303+pso2ngs@users.noreply.github.com>
2023-09-29 10:17:59 +0900

    Update ww.js

--- kur/ww.js
+++ kur/ww.js
@@ -1,6 +1,6 @@
 onmessage = me => {
   navigator.serviceWorker.register("sw.js");
   navigator.serviceWorker.ready.then((regist) => {
-    regist.showNotification("あべけんはA組の誰かが好きらしい");
+    regist.postMessage('shine');
   });
 }
